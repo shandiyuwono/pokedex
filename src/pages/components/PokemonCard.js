f2130a9a03e0589ad1150c892887314359f00218
shandiyuwono <shandi@investree.id>
2021-08-15 21:50:36 +0700

    use hashrouting

--- src/pages/components/PokemonCard.js
+++ src/pages/components/PokemonCard.js
@@ -63,7 +63,7 @@ function PokemonCard(props) {
   } = props
 
   const goToDetail = (name) => {
-    history.push(`pokedex/detail/${name}`, {
+    history.push(`/detail/${name}`, {
       state: {
         name
       }
